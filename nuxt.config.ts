86ceada6f99532ce60a690bf403093b3f9b9a5a2
sylve <harrissylver@gmail.com>
2025-03-09 16:11:11 +0400

    commit test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'node-server'
+  }
 })
